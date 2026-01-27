d467b69e186cb0a3c8b5d519248239f6a032d7a6
egrevs <egorevseenko203@gmail.com>
2026-01-27 14:28:14 +0200

    todo "lower" sql completed

--- src/main/java/com/egrevs/project/lanittest/repository/CarRepository.java
+++ src/main/java/com/egrevs/project/lanittest/repository/CarRepository.java
@@ -10,6 +10,6 @@ public interface CarRepository extends JpaRepository<Car, Long> {
 
     long count();
 
-    @Query(value = "select count(distinct vendor) from Car")
+    @Query(value = "select count(distinct (lower(c.vendor))) from Car as c")
     long countByVendor();
 }
