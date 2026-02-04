647a64293e076664c59b4c83a2db7bbcea3a7095
egrevs <egorevseenko203@gmail.com>
2026-02-04 17:26:10 +0300

    LocalDate

--- src/main/java/com/egrevs/project/lanittest/dto/CreatePersonRequest.java
+++ src/main/java/com/egrevs/project/lanittest/dto/CreatePersonRequest.java
@@ -3,6 +3,7 @@ package com.egrevs.project.lanittest.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public record CreatePersonRequest(
@@ -12,6 +13,6 @@ public record CreatePersonRequest(
         String name,
         @NotNull
         @JsonFormat(pattern = "dd.MM.yyyy")
-        Date birthday
+        LocalDate birthday
 ) {
 }
